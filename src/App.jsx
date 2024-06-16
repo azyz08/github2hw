0e13182482083eb4c465ba9802db9bfa06e22e35
Abduxabir2024 <abduqayumovabduxabir2@gmail.com>
2024-06-16 21:52:47 +0500

    end the boldi

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,71 @@
-import './App.sss'
+import './App.scss'
+import Btn from './buttonf'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Lorem ipsum dolor sit.</h1>
+      <div className="card">
+        <p><span>Illud decore voluptaria has at. Hinc invenire atomorum no vel.</span> Ut vis nullam blandit neglegentur, omittam perpetua voluptatum qui eu. Iusto laoreet suscipit vis ad, ad ferri tempor duo.</p>
+      </div>
+      <div className="card1">
+        <div className="card2">
+          <img src="./ori.png" alt="" />
+        </div>
+        <div className="card3">
+          <p><span>Ex tempor euismod sapientem ouis lexor pro.</span>Ad vix dicta iudico singulis, ius ex mollis veritus.</p>
+          <h5>Possit tritani consectetuer eu his, nusquam minimum ut per, eu eos ocurreret splendide hendrerit. Dicant placerat democritum eos id, sit mollis invidunt mediocritatem ei, ne fuisset dolores periculis sed.</h5>
+        </div>
+      </div>
+      <div className="card1">
+        <div className="card3">
+          <p><span>Te elit antiopam vix zril recusabo ut pri. </span>Quas suscipit ad mea verear vivendo tincidunt.</p>
+          <h5>Pri cu dico labores officiis, odio principes complectitur ad sea. Sea id doctus forensibus, nec lorem vocent aliquam eu. Aliquid definitiones id cum, ad meliore perpetua referrentur sed. Quas suscipit ad mea verear vivendo tincidunt.</h5>
+        </div>
+        <div className="card2">
+          <img src="./yongo.png" alt="" />
+        </div>
+      </div>
+      <div className="chi">
+        <h1>Wanna know more about the Premium deal?</h1>
+        <p>See the full range of Premium</p>
+      </div>
+      <div className="card4">
+        <div className="card5">
+          <span>Party On --</span>
+          <p><h3>Quas suscipit ad mea.</h3> Pri cu dico labores officiis odio principes complectitur ad sea.</p>
+        </div>
+        <div className="card6">
+        <Btn/>
+        </div>
+      </div>
+      <div className="end">
+        <div className="cards">
+          <img src="./logoo.png" alt="" />
+          <p>© 2018 Spoon Inc, All rights reserved.</p>
+        </div>
+        <div className="cards1">
+          <ul>
+            <li><a href="#">Features</a></li>
+            <li><a href="#">Premium</a></li>
+            <li><a href="#">Pricing</a></li>
+          </ul>
+          <ul>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+          <ul>
+            <li><a href="#">Image License Info</a></li>
+            <li><a href="#">Powered by Webflow</a></li>
+          </ul>
+          <div className="bir">
+            <img src="./facebook.png" alt="" />
+            <img src="./twiter.png" alt="" />
+            <img src="./insta.png" alt="" />
+          </div>
+        </div>
+      </div>
     </>
   )
 }
